7b337603afe784a968484cec97f3d783adce8f6c
Kristoffer Andersson <6058164+kod-kristoff@users.noreply.github.com>
2019-10-18 08:46:02 +0200

    Add first code

---
+++ run.py
@@ -0,0 +1,18 @@
+import json
+import os
+
+import urllib3
+
+
+PORT = os.environ.get("ES_PORT", 9200)
+
+
+def main():
+    http = urllib3.PoolManager()
+    r = http.request("GET", f"localhost:{PORT}")
+    assert r.status == 200
+    data = json.loads(r.data.decode("utf-8"))
+    print(f"data = {data}")
+
+if __name__ == "__main__":
+    main()
